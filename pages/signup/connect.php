4276dd9305c0ff02c906fb91b5a8f039731b09bb
nikesh <nikeshsplash@gmail.com>
2024-01-28 09:47:49 +0530

    database connected to xamp server

--- pages/signup/connect.php
+++ pages/signup/connect.php
@@ -1,43 +1,21 @@
 <?php
-
-    //database connection
-    $conn  = new mysqli('localhost', root ,'');
-    if(isset($_POST['signinBtn'])){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $_sql= "SELECT *FROM Netflix.registration WHERE email = '$email'";
-        $result = mysqli_query($conn,$_sql);
-        while($row = mysqli_fetch_assoc($result)){
-            $resultPassword = $row['password'];
-            if($password = $resultPassword){
-                header('Location:welcomepage.html');
-            }else{
-                echo "<script> 
-                    alert('Login failed');    
-                </script>";
-            }
-        }
-    }
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
 
 
-
-
-
-
-
-    if($conn->connect_error){
-        die('COnnection failed'. $conn->connect_error);
-    }else{
-        $stmt = $conn->prepare("insert into registration(email,password)
-            values(?,?)");
-            $stmt->bind_param("ss", $email,$password);
-            $stmt->execute();
-            echo "Sign in succesful";
-            $stmt->close();
-            $conn->close();
-
-    }
-
- 
+	// Database connection
+	$conn = new mysqli('localhost','root','','Netflix');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(email, password) values( ?, ?)");
+		$stmt->bind_param("ss",  $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
 ?>
