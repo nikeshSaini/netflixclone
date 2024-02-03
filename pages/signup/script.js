932bae7ccafd8687a6e5c88dc91289b520b17353
nikesh <nikeshsplash@gmail.com>
2024-02-04 00:42:51 +0530

    removed /netlfixclone_own from all directory file

--- pages/signup/script.js
+++ pages/signup/script.js
@@ -1,3 +1,3 @@
 document.getElementById('logo').addEventListener('click', function(){
-    window.location.href = '/netflixclone_own/index.html';
+    window.location.href = '/index.html';
 });
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('signinbtn').addEventListener('click', function(){
-    window.location.href = '/netflixclone/pages/signup/singup.html';
+    window.location.href = '/pages/signup/singup.html';
 });
 
 document.getElementById('logo').addEventListener('click', function(){
-    window.location.href = '/netflixclone_own/index.html';
+    window.location.href = '/index.html';
 });
 
 document.getElementById('startingSignIn').addEventListener('click', function(){
-    window.location.href = '/netflixclone_own/pages/singin/singin.html';
+    window.location.href = '/pages/singin/singin.html';
 });
 
 //important
