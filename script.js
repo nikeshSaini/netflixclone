e67bb0041bcb864624ead2e9cfaa913b394b3f87
nikesh <nikeshsplash@gmail.com>
2024-02-03 11:34:18 +0530

    final javascript

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ document.getElementById('logo').addEventListener('click', function(){
 document.getElementById('startingSignIn').addEventListener('click', function(){
     window.location.href = '/netflixclone_own/pages/singin/singin.html';
 });
+
+//important
+
+
+  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const showInfoButtons = document.querySelectorAll('.btn');
+    const infoDivs = document.querySelectorAll('.info');
+  
+    showInfoButtons.forEach(function(button, index) {//loop for all
+      button.addEventListener('click', function() {
+        // Toggle the display style of the corresponding infoDiv
+        infoDivs[index].style.display = infoDivs[index].style.display === 'none' ? 'block' : 'none';
+      });
+    });
+  });
+  
+
